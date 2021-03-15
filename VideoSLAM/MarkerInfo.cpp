eaec377c69723a4706390efa8de3be4a7e64542e
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2021-03-15 18:12:42 +0100

    Basic code framework
    Currently, the code will generate the Aruco markers, that can be printed out as well as play the input video, named "InputVideos/Input_2.mp4". Note that the video is not currently added in the repository. The video will be displayed, as will be the Aruco markers (well enough) visible in the video itself, frame by frame. The rest of the code has yet to be fully implemented.

---
+++ VideoSLAM/MarkerInfo.cpp
@@ -0,0 +1,10 @@
+﻿#include "stdafx.h"
+#include "MarkerInfo.h"
+
+MarkerInfo::MarkerInfo() {
+    this->visible = false;
+    this->previous_marker_id = -1;
+    this->marker_id = -1;
+}
+
+MarkerInfo::~MarkerInfo() = default;
---
+++ VideoSLAM/MarkerInfo.h
@@ -0,0 +1,54 @@
+﻿#ifndef MARKER_INFO_H
+#define MARKER_INFO_H
+
+#include <opencv2\opencv.hpp>
+
+
+class MarkerInfo {
+public:
+    // attributes
+    bool visible;
+    int previous_marker_id;
+    int marker_id;
+
+    // relative to world transformation
+    float world_position_x;
+    float world_position_y;
+    float world_position_z;
+    float world_orientation_x;
+    float world_orientation_y;
+    float world_orientation_z;
+    float world_orientation_w;
+
+    // relative to previous marker transformation
+    float to_prev_position_x;
+    float to_prev_position_y;
+    float to_prev_position_z;
+    float to_prev_orientation_x;
+    float to_prev_orientation_y;
+    float to_prev_orientation_z;
+    float to_prev_orientation_w;
+
+    // current camera pose
+    // TODO: chose which is actually needed
+    float current_camera_pose_position_x;
+    float current_camera_pose_position_y;
+    float current_camera_pose_position_z;
+    /* NOTE: ignore this untill you absolutely need this (and then implement rotation matrix to quaternion conversion :) )
+    float current_camera_pose_orientation_x;
+    float current_camera_pose_orientation_y;
+    float current_camera_pose_orientation_z;
+    float current_camera_pose_orientation_w;
+    */
+    cv::Mat current_camera_pose_position;
+    cv::Mat current_camera_pose_orientation;
+
+
+    // constructors & deconstructors
+    MarkerInfo();
+    virtual ~MarkerInfo();
+
+    // methods
+};
+
+#endif
