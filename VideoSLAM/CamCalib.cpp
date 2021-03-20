9c460b2fcb04d444c8faedf70f0fdd7c2747133c
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2021-03-20 13:36:08 +0100

    Move calibration code to CamCalib
    This should allow code experimentation and adition, without making the code in the main file too messy.

---
+++ VideoSLAM/CamCalib.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "CamCalib.h"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/core/quaternion.hpp>
+
+using namespace std;
+using namespace cv;
+
+int CamCalib::myCalibrateCamera() {
+    // TODO set these to accurate 
+    int num_images = 63;        // number of images used for calibration
+    Size cal_grid_size(5, 7);   // num cols, num rows; on the pattern used for calibration
+    double cal_dot_r = 0.006;   // single dot on grid radius in mm
+    vector<vector<Point2f>> imagePoints;
+
+    String window_name = "Calibration Preview";
+    namedWindow(window_name, WINDOW_NORMAL);
+
+    int count = 0;
+    for(int img_num = 0; img_num < num_images; img_num++) {
+        // open image files
+        string file_name = format("Calibration/CAL_IMG (%d).jpg", img_num);
+        Mat cal_image = imread(file_name, IMREAD_GRAYSCALE);
+
+        // pre-process image
+        // threshold(cal_image, cal_image, 127, 255, THRESH_OTSU);
+
+        // flip image
+        // flip(cal_image, cal_image, 0);
+
+        // find pattern
+        vector<Point2f> pointBuf;
+        bool found = findCirclesGrid(cal_image, cal_grid_size, pointBuf);
+
+        if(found) {
+            imagePoints.push_back(pointBuf);
+            cout << "Found circle grid, IMG: " << img_num << endl;
+
+            drawChessboardCorners(cal_image, cal_grid_size, Mat(pointBuf), found);
+            count++;
+        } else {
+            cout << "Not found :(" << endl;
+        }
+
+        // display image
+        imshow(window_name, cal_image);
+        char key = (char) waitKey(1);
+        if(key == 27) break;
+    }
+
+    // TODO: runCalibrationAndSave
+    return count;
+}
+
+CamCalib::CamCalib() {}
+CamCalib::~CamCalib() = default;
---
+++ VideoSLAM/CamCalib.h
@@ -0,0 +1,11 @@
+#ifndef CAMCALIB_H
+#define CAMCALIB_H
+
+class CamCalib {
+    public:
+        int myCalibrateCamera();
+        CamCalib();
+        virtual ~CamCalib();
+};
+
+#endif // CAMCALIB_H
