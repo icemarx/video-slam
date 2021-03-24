f899d1cd0c33152bdecf5656b874b2e2ffa7cad0
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2021-03-24 16:04:20 +0100

    Remove explicit variables for marker position and orientation

--- VideoSLAM/MarkerInfo.h
+++ VideoSLAM/MarkerInfo.h
@@ -10,31 +10,15 @@ public:
     int marker_id;
 
     // relative to world transformation
-    float world_position_x;
-    float world_position_y;
-    float world_position_z;
-    float world_orientation_x;
-    float world_orientation_y;
-    float world_orientation_z;
-    float world_orientation_w;
+    cv::Vec3d world_position;
+    cv::Quat<double> world_orientation;
 
     // relative to previous marker transformation
-    float to_prev_position_x;
-    float to_prev_position_y;
-    float to_prev_position_z;
-    float to_prev_orientation_x;
-    float to_prev_orientation_y;
-    float to_prev_orientation_z;
-    float to_prev_orientation_w;
+    cv::Vec3d to_prev_position;
+    cv::Quat<double> to_prev_orientation;
 
     // current camera pose
-    // TODO: chose which is actually needed
-    float current_camera_pose_position_x;
-    float current_camera_pose_position_y;
-    float current_camera_pose_position_z;
-
-    cv::Mat current_camera_pose_position;
-    // Mat current_camera_pose_orientation;
+    cv::Vec3d current_camera_pose_position;
     cv::Quat<double> current_camera_pose_orientation;
 
 
